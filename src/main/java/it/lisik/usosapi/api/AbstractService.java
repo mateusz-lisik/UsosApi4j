d2c2bb514c8bd4ebed7ee2da857c696119c65f70
Mateusz Lisik <matt@procreative.eu>
2015-07-31 23:53:01 -0400

    Added support for fetching client from service

--- src/main/java/it/lisik/usosapi/api/AbstractService.java
+++ src/main/java/it/lisik/usosapi/api/AbstractService.java
@@ -3,14 +3,22 @@ package it.lisik.usosapi.api;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
+import it.lisik.usosapi.UsosClient;
 
 public abstract class AbstractService {
     protected final HttpRequestFactory requestFactory;
     protected final String applicationUrl;
+    protected final UsosClient client;
+
+    @Inject
+    public AbstractService(HttpRequestFactory requestFactory, @Named("ApplicationUrl") String applicationUrl, UsosClient client) {
+        this.requestFactory = requestFactory;
+        this.applicationUrl = applicationUrl;
+        this.client = client;
+    }
 
     @Inject
     public AbstractService(HttpRequestFactory requestFactory, @Named("ApplicationUrl") String applicationUrl) {
-        this.requestFactory = requestFactory;
-        this.applicationUrl = applicationUrl;
+        this(requestFactory, applicationUrl, null);
     }
 }
